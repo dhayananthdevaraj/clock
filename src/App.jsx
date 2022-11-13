131157663faaac7480dc65735fff6f319b6d0a2e
dhayananthdevaraj <dhayan.d3@gmail.com>
2022-11-13 09:18:47 +0530

    clock face

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Clock from './Clock';
+function App() {
+
+    return(
+      <>
+      <div className="container">
+        <Clock />
+       </div>
+      </>
+    )
+}
+
+export default App
